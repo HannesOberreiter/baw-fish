231c8c863cf8fea4d0eb869474cc3780cfa8552a
HannesOberreiter <HannesOberreiter@users.noreply.github.com>
2023-06-04 16:33:47 +0000

    deploy: bd1be0165ae039b4531e0a2770471ae96fa90b5e

--- assets/EggsCounterPage.467f6519.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.2ebdf481.js";import{_ as t,q as r,s as a,t as o,N as s}from"./index.289bc600.js";import"./render.a937a657.js";const c={};function n(_,f){return r(),a(e,null,{default:o(()=>[s(" Under development ... ")]),_:1})}var p=t(c,[["render",n]]);export{p as default};
