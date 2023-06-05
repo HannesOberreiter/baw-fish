3bc82dbb08ef27ffe548fc37aead9152188f1302
HannesOberreiter <hoberreiter@gmail.com>
2023-06-05 10:44:31 +0200

    docs: :memo: change up text for under construction

--- app/src/pages/EggsCounterPage.vue
+++ app/src/pages/EggsCounterPage.vue
@@ -1,3 +1,8 @@
 <template>
-  <q-page> Under development ... </q-page>
+  <q-page>
+    <div class="text-center text-h3 text-orange q-mt-xl">
+      <q-icon name="construction"></q-icon>
+      Under development ...
+    </div>
+  </q-page>
 </template>
